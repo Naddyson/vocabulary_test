71a673d9984ea8438ec5b7e24ae3862d4281a3f0
Корнак Евгений < >
2016-10-21 06:52:22 +0300

    alpha version 1.0

--- js/script.js
+++ js/script.js
@@ -1,6 +1,13 @@
+$("textarea.main_input").keyup(function(){ //пока нет текста кнопка не работает
+		var empty=true;
+		if ($(".main_input").val() == "") empty=false;
+			else empty = true;
+		if (empty) $(".button_start").prop("disabled",false);
+			else $(".button_start").prop("disabled",true);
+	});
 
-
-function makeArray(text){
+function makeArray(text){ 
+//извлекаем из поля текст и разбиваем его на объекты
 	var arr = [];
 
 	var last = 0;console.log(text.length);
@@ -21,10 +28,10 @@ function makeArray(text){
 						var translation = $.trim(text.substring(dash,j).toLowerCase());
 						console.log("translation="+translation);
 					//	task.translation = translation;
-					 arr.push({
+					arr.push({
 								'word': word,
 								'translation': translation
-						});
+					});
 						last = j+1;
 						break;
 					} else j++;
@@ -35,55 +42,103 @@ function makeArray(text){
 	return arr;
 	
 }
-function put_task(task){
-	
-	console.log("task="+task);
-	$(".word").text(task.translation);
-	$(".answer_form").attr("disabled",false);
-	$(".button_submit").click(function(){
-		$(".answer_form").attr("disabled",true);
-		
-		var answer = $(".answer_form").val();
-		console.log(answer);
-		if ($.trim(answer)==task.word){
-			$(".word").removeClass("label-default");
-			$(".word").addClass("label-success");
-			$(".button_next").attr("disabled",false);
-			return true;
-		}
-		else {
-			$(".word").removeClass("label-default");
-			$(".word").addClass("label-danger");
-			$(".button_next").attr("disabled",false);
-			$(".answer").show();
-			$(".answer").text(task.word+" - "+task.translation);
-			return false;
-		}
-		
-	});
-}
 
+var right = 0;
+var wrong = [];
+var task_array;
 var main = function () {
 	var i = 0;
-	var score = [];
-	var task_array;
+	//$(".button_restart").prop("disabled",false);
+	
+	var split;
+	var current_progress = 0;
+
+	
 	$(".button_start").click(function(){
 		task_array = makeArray($("textarea.main_input").val());
-		$(".one").slideUp(400);
-		$(".two").hide();
-		$(".test").show();
-		put_task(task_array[i]); //здесь array НЕ ОПРЕДЕЛЕН!
-		i++;
-		
+		if (task_array.length != 0){
+		$(".progress").show();
+		split = 100 / task_array.length;
+		$(".one").slideUp(400); //убираем описание
+		$(".two").hide(); //убираем поле ввода
+		$(".test").show(); //показываем окно тестирования
+		$(".word").text(task_array[i].translation);
+	} else alert("You put something wrong!")
 	});
-	$(".button_next").click(function(){
-			console.log("i"+i);
-			put_task(task_array[i]);
-			i++;
-		});
-	
+
+
 
 	
+	$(".button_next").click(function(){
+		i++;
+		current_progress=current_progress+split;
+		$(".progress-bar").css("width",current_progress+"%");
+		if (i != task_array.length){
+			$(".button_next").attr("disabled",true);
+			console.log("current_progress="+current_progress);
+			$(".word").text(task_array[i].translation);
+			$(".answer_form").attr("disabled",false);
+			$(".answer_form").val("");
+			
+			set_color_default();
+		} else {
+			game_over();
+
+		}
+	});
+	
+	$(".button_submit").click(function(){
+
+		$(".answer_form").attr("disabled",true);
+		var answer = $(".answer_form").val();
+		if ($.trim(answer)==task_array[i].word){
+			set_color_green();
+			$(".button_next").attr("disabled",false);
+			right++;
+		}
+		else {
+			set_color_red();
+			$(".button_next").attr("disabled",false);
+			$(".answer").show();
+			$(".answer").text(task_array[i].word+" - "+task_array[i].translation);
+			wrong.push(task_array[i]);
+		}
+	});
+	$(".button_restart").click(function(){
+		new main;
+	});
+}
+function set_color_default(){
+	$(".word").removeClass("label-danger");
+	$(".word").removeClass("label-success");
+	$(".word").addClass("label-default");
+}
+function set_color_red(){
+	$(".word").removeClass("label-default");
+		$(".word").addClass("label-danger");
+}
+function set_color_green(){
+	$(".word").removeClass("label-default");
+	$(".word").addClass("label-success");
+}
+
+function game_over(){
+	$(".test").hide();
+	$(".game_over").show();
+	$("<h1>").text(right+" correct answers out of "+task_array.length+".").appendTo(".game_over");
+	$("<h2>").text("Wrong answers:").appendTo(".game_over");
+	for (var i = 0; i < wrong.length; i++) {
+		$("<h4>").text(wrong[i].word+" - "+wrong[i].translation).appendTo(".game_over");
+	}
+	$("<form>").addClass("form-inline").appendTo(".game_over");
+
+	$("<button>").addClass("btn btn-success button_restart").text("Try again").appendTo(".form-inline");
+	$("<h3>").text("For new input text reload the page.").appendTo(".form-inline");
+
 
 }
-$(document).ready(main);
+$(document).ready(new main());
+$(document).ready(function(){
+	
+		
+});
